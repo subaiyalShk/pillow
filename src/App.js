6bdc0ac2cff6fdc50bdde8d693a5ef358d78aa25
Subaiyal Sheikh <subaiyalshk@gmail.com>
2024-03-27 03:08:48 -0500

    basic world map setup with search and geolocator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Map, {GeolocateControl} from 'react-map-gl';
+import GeocoderControl from './GeocoderControl';
+
 
 function App() {
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [error, setError] = useState(null);
+
+
+  useEffect(() => {
+    const getLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            setLatitude(position.coords.latitude);
+            setLongitude(position.coords.longitude);
+          },
+          (error) => {
+            setError(error.message);
+          }
+        );
+      } else {
+        setError('Geolocation is not supported by this browser.');
+      }
+    };
+
+    getLocation();
+  }, []); // Empty dependency array ensures this is only run once
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX_KEY}
+        initialViewState={{
+          longitude: longitude,
+          latitude: latitude,
+          zoom: 14
+        }}
+        style={{width: '100%', height: "100vh"}}
+        mapStyle="mapbox://styles/subaiyalshk/clu9dc7cw01l201qqdw27ep6d"
+      >
+        <GeolocateControl position='bottom-right'/>
+        <GeocoderControl mapboxAccessToken={process.env.REACT_APP_MAPBOX_KEY} position="top-left" />
+      </Map>
     </div>
   );
 }
