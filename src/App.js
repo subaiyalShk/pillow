c8428d17991e30c7808abf315008df4f736cdef9
Subaiyal Sheikh <subaiyalshk@gmail.com>
2024-03-27 20:20:10 -0500

    metamask-sdk, geocoder search basic, and properties slider

--- src/App.js
+++ src/App.js
@@ -1,51 +1,76 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Map, {GeolocateControl} from 'react-map-gl';
-import GeocoderControl from './GeocoderControl';
-
+import GeocoderControl from './components/GeocoderControl';
+import { Button, Box, Card, TabNav } from '@radix-ui/themes';
+import { useSDK } from "@metamask/sdk-react";
+import PropertySlider from './components/PropertySlider';
 
 function App() {
-  const [latitude, setLatitude] = useState(null);
-  const [longitude, setLongitude] = useState(null);
-  const [error, setError] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [view, setView] = useState('buy');
+  const { sdk, chainId } = useSDK();
+
+  const connectToWeb3 = async () => {
+    try {
+        const accounts = await sdk?.connect();
+        console.log(accounts)
+        setAccount(accounts?.[0]+'');
+    } catch (err) {
+        console.warn("failed to connect..", err);
+    }
+  };
+
+  const disconnectFromWeb3 = async () => {
+    sdk.terminate()
+    setAccount(null);
+  }
 
 
-  useEffect(() => {
-    const getLocation = () => {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            setLatitude(position.coords.latitude);
-            setLongitude(position.coords.longitude);
-          },
-          (error) => {
-            setError(error.message);
-          }
-        );
-      } else {
-        setError('Geolocation is not supported by this browser.');
-      }
-    };
-
-    getLocation();
-  }, []); // Empty dependency array ensures this is only run once
-
+  const changeView = (e, value) => {
+    e.preventDefault();
+    setView(value);
+  }
 
   return (
     <div className="App">
+      <div id="top-container">
+        {account ?
+          <Button className="login" onClick={disconnectFromWeb3}>{chainId} | {`${account.slice(0,5)}...${account.slice(account.length-5,account.length)}`}</Button>
+            :
+          <Button className="login" onClick={connectToWeb3}>Connect</Button>
+        }
+        <Card className='menu-tabs'>
+          <TabNav.Root>
+            <TabNav.Link onClick={(e)=>changeView(e,'account')} href="#" active={view==="account"?true:false}>
+              Claim
+            </TabNav.Link>
+            <TabNav.Link onClick={(e)=>changeView(e,'buy')} href="#" active={view==="buy"?true:false}>Buy</TabNav.Link>
+            <TabNav.Link onClick={(e)=>changeView(e,'sell')} href="#" active={view==="sell"?true:false}>Sell</TabNav.Link>
+          </TabNav.Root>
+        </Card>
+        <div className='spacer'>
+
+        </div>
+      </div>
       <Map
         mapboxAccessToken={process.env.REACT_APP_MAPBOX_KEY}
         initialViewState={{
-          longitude: longitude,
-          latitude: latitude,
+          longitude: -96.808891,
+          latitude: 32.779167,
           zoom: 14
         }}
-        style={{width: '100%', height: "100vh"}}
+        style={{width: '100%', height: "105vh"}}
         mapStyle="mapbox://styles/subaiyalshk/clu9dc7cw01l201qqdw27ep6d"
       >
         <GeolocateControl position='bottom-right'/>
         <GeocoderControl mapboxAccessToken={process.env.REACT_APP_MAPBOX_KEY} position="top-left" />
       </Map>
+      <Box className='data-display-container'>
+        <Card className='data-display-card' >
+        <PropertySlider/>
+        </Card>
+      </Box>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
+import { MetaMaskProvider } from "@metamask/sdk-react";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MetaMaskProvider
+      debug={true}
+      sdkOptions={{
+          dappMetadata: {
+              name: "Example React Dapp",
+              url: window.location.href,
+          },
+          infuraAPIKey: process.env.REACT_APP_INFURA_API_KEY,
+          // Other options
+      }}
+    >
+    <Theme appearance="dark" accentColor="lime" grayColor="slate" radius="large">
+      <App />
+    </Theme>
+    </MetaMaskProvider>
   </React.StrictMode>
 );
 
